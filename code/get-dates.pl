2f10f2a3ac6a7b955ea318e7c6128c7ee548a758
JJ Merelo <jjmerelo@gmail.com>
2018-01-29 10:41:58 +0100

    Tried and tested first version

--- code/get-dates.pl
+++ code/get-dates.pl
@@ -3,9 +3,25 @@
 use strict;
 use warnings;
 
+use v5.14;
+
 use lib qw(/home/jmerelo/Code/CPAN/perl-git-commit);
 
-use Git::Repo::Commit;
+use Git::Repo::Commits;
 
-my $repo = shift || ".";
-my $file = shift || die "I need a file to work on it";
+my $home = shift || "/home/jmerelo/txt/docencia";
+
+my %repos = ( 'infraestructura-virtual/IV16-17' => ['practicas',
+						    ['0','1','2','3','4','5']]);
+
+for my $r ( keys %repos ) {
+  my $preffix = $repos{$r}[0];
+  for my $h ( @{$repos{$r}[1]} ) {
+    my @files = ("$preffix/$h.md");
+    my $commits_obj = new Git::Repo::Commits "$home/$r", \@files;
+    my @commit_dates = map( $_->{'commit_date'}, @{$commits_obj->commits});
+    for my $d ( @commit_dates ) {
+      say "$r, $h, $d";
+    }
+  }
+}
