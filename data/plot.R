5ade2f3bb5817f3f75fd5d08dd6b0c8115613707
JJ Merelo <jjmerelo@gmail.com>
2018-01-30 09:47:35 +0100

    Add plotting

--- data/plot.R
+++ data/plot.R
@@ -2,4 +2,5 @@ library("lubridate")
 library("ggplot2")
 dates <- read.csv("iv-changes.csv")
 dates$Day <- as.Date(dates$Day,format="%m/%d/%Y")
+ ggplot(dates,aes(x=Day,y=Changes,group=Class,color=Milestone))+geom_point()
 
